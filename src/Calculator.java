df6aa8248bce992a3ecb02cf554996e743a41377
Serg <freekey@list.ru>
2023-08-30 17:33:53 +0400

    errors corrected

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,19 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+
+    BinaryOperator<Integer> devide = (x, y) -> {
+        try {
+            return x / y;
+        } catch (ArithmeticException exception) {
+            System.out.println("ОШИБКА. Деление на 0.");
+        } finally {
+            System.exit(1);
+        }
+        return 0;
+    };
+
+
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,9 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1,1);
-        try {
-            int c = calc.devide.apply(a, b); // выкидывается unchecked ошиба "деление на ноль" (java.lang.ArithmeticException: / by zero)
-            calc.println.accept(c);
-        } catch (ArithmeticException exception) {
-            System.out.println("ОШИБКА. Деление на 0.");
-        }
-
-
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b); // выкидывается unchecked ошиба "деление на ноль" (java.lang.ArithmeticException: / by zero)
+        calc.println.accept(c);
 
 
     }
